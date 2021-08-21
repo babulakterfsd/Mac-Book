3c524545702c01dda9939c9f4fc91e186dcbcacc
Babul Akter <babulakterfsd@gmail.com>
2021-08-21 20:11:39 +0600

    promo code bug fixed

--- js/main.js
+++ js/main.js
@@ -5,7 +5,8 @@ const storageCost = document.getElementById('extra-storage-cost');
 const deliveryCharge = document.getElementById('delivery-charge');
 const allCost = document.getElementById('total-price');
 const total = document.getElementById('total');
-const promoInput = document.getElementById('pomo-input');
+const promoInput = document.getElementById('promo-input');
+const applyButton = document.getElementById('applyButton');
 
 
 // function for Total Price 
@@ -16,50 +17,50 @@ function totalPrice(){
 }
 
 // memory price calculator
-function memoryTest(id, price){
+function memoryPrice(id, price){
     document.getElementById(id).addEventListener('click',function(){
         memoryCost.innerText = price;
         totalPrice();
         total.innerText = totalPrice();
     })
 }
-memoryTest('free-memory','0');
-memoryTest('cost-memory','180');
+memoryPrice('free-memory','0');
+memoryPrice('cost-memory','180');
 
 
 // storage price calculator
-function storageTest(id, price){
+function storagePrice(id, price){
     document.getElementById(id).addEventListener('click',function(){
         storageCost.innerText = price;
         totalPrice();
         total.innerText = totalPrice();
     })
 }
-storageTest('ssd-256gb', '0');
-storageTest('ssd-512gb', '100');
-storageTest('ssd-1tb', '180');
+storagePrice('ssd-256gb', '0');
+storagePrice('ssd-512gb', '100');
+storagePrice('ssd-1tb', '180');
 
 
 // delivery charge calculator
-function deliveryTest(id, price){
+function deliveryCostCalc(id, price){
     document.getElementById(id).addEventListener('click',function(){
         deliveryCharge.innerText = price;
         totalPrice();
         total.innerText = totalPrice();
     })
 }
-deliveryTest('free-delivery', '0');
-deliveryTest('cost-delivery', '20');
+deliveryCostCalc('free-delivery', '0');
+deliveryCostCalc('cost-delivery', '20');
 
 
 // total calculation with pomo code
 function promoCode(){
     const promoValue = promoInput.value;
     if(promoValue == 'stevekaku'){
-        const pomoCalculation = (parseInt(total.innerText) * 20) / 100;
-        total.innerText = parseInt(total.innerText) - pomoCalculation;
+        const discount = (parseInt(allCost.innerText) * 20) / 100;
+        total.innerText = parseInt(allCost.innerText) - discount;
         promoInput.value = '';
     }else{
-        alert('Please Enter Correct Pomo Code')
+        alert('Please Enter Correct Promo Code');
     }
 }
