43d2349f9c4ea5f4ce9a87426fe6f0e8d588c8f9
Babul Akter <babulakterfsd@gmail.com>
2021-08-21 15:14:11 +0600

    minor bug fixed in promo code

--- js/main.js
+++ js/main.js
@@ -1,83 +1,65 @@
-//all target html elements
+// Global Variables
 const bestPrice = document.getElementById('best-price');
 const memoryCost = document.getElementById('extra-memory-cost');
 const storageCost = document.getElementById('extra-storage-cost');
 const deliveryCharge = document.getElementById('delivery-charge');
 const allCost = document.getElementById('total-price');
-const discountedTotal = document.getElementById('total-with-discount');
+const total = document.getElementById('total');
+const promoInput = document.getElementById('pomo-input');
 
 
 // function for Total Price 
-function updateTotalPrice(){
+function totalPrice(){
     const totalCost = parseInt(bestPrice.innerText) + parseInt(memoryCost.innerText) + parseInt(storageCost.innerText) + parseInt(deliveryCharge.innerText);
     allCost.innerText = totalCost;
     return totalCost;
 }
 
-
-// event linstener for Memory
-document.getElementById('free-memory').addEventListener('click',function(){
-    memoryCost.innerText = '0';
-    updateTotalPrice();
-    grandTotalWithDiscount();
-})
-
-document.getElementById('cost-memory').addEventListener('click',function(){
-    memoryCost.innerText = '180';
-    updateTotalPrice();
-    grandTotalWithDiscount();
-})
-
-
-// event linstener for Storage
-document.getElementById('ssd-256gb').addEventListener('click',function(){
-    storageCost.innerText = '0'
-    updateTotalPrice();
-    grandTotalWithDiscount();
-})
-
-document.getElementById('ssd-512gb').addEventListener('click',function(){
-    storageCost.innerText = '100'
-    updateTotalPrice();
-    grandTotalWithDiscount();
-})
-
-document.getElementById('ssd-1tb').addEventListener('click',function(){
-    storageCost.innerText = '180'
-    updateTotalPrice();
-    grandTotalWithDiscount();
-})
-
-
-// event linstener for delivery option
-document.getElementById('free-delivery').addEventListener('click',function(){
-    deliveryCharge.innerText = '0';
-    updateTotalPrice();
-    grandTotalWithDiscount();
-})
-
-document.getElementById('cost-delivery').addEventListener('click',function(){
-    deliveryCharge.innerText = '20';
-    updateTotalPrice();
-    grandTotalWithDiscount();
-})
-
-
-// Function for updating Grand Total price without discount
-function grandTotalWithDiscount() {
-   discountedTotal.innerText = updateTotalPrice()
+// memory price calculator
+function memoryTest(id, price){
+    document.getElementById(id).addEventListener('click',function(){
+        memoryCost.innerText = price;
+        totalPrice();
+        total.innerText = totalPrice();
+    })
 }
+memoryTest('free-memory','0');
+memoryTest('cost-memory','180');
 
 
-//Grand total with discount
+// storage price calculator
+function storageTest(id, price){
+    document.getElementById(id).addEventListener('click',function(){
+        storageCost.innerText = price;
+        totalPrice();
+        total.innerText = totalPrice();
+    })
+}
+storageTest('ssd-256gb', '0');
+storageTest('ssd-512gb', '100');
+storageTest('ssd-1tb', '180');
+
+
+// delivery charge calculator
+function deliveryTest(id, price){
+    document.getElementById(id).addEventListener('click',function(){
+        deliveryCharge.innerText = price;
+        totalPrice();
+        total.innerText = totalPrice();
+    })
+}
+deliveryTest('free-delivery', '0');
+deliveryTest('cost-delivery', '20');
+
+
+// total calculation with pomo code
 function promoCode(){
-    const promoCode = document.getElementById('promocode');
-    const promoValue = promoCode.value;
+    const promoValue = promoInput.value;
     if(promoValue == 'stevekaku'){
-        const discountRate = (updateTotalPrice() / 100) * 20;
-        discountedTotal.innerText = updateTotalPrice() - discountRate;
-        promoCode.value = '';
+        const pomoCalculation = (parseInt(total.innerText) * 20) / 100;
+        total.innerText = parseInt(total.innerText) - pomoCalculation;
+        pomoInput.value = '';
     }else{
-        alert('Please Enter Correct Promo Code')
+        alert('Please Enter Correct Pomo Code')
     }
 }
